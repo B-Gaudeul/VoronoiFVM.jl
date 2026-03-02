1f1de8de244a1475000445ebe79b646aa582ce35
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2026-03-02 16:39:36 +0100

    fix ci and docs issues (#259)
    - Remove native Pluto environments from notebooks to speed up documentation generation. It seems that conditional activation of the docs environment happened only after the notebook environment was instantiated
    - avoid generating some of the notebook htmls due to mystical termination by github CI
    - Failing of Example440 is ignored...

--- docs/make.jl
+++ docs/make.jl
@@ -44,32 +44,8 @@ function make(;
     ]
 
 
-    if with_notebooks
-        @info "Start notebook evaluation"
-        notebooks = [
-            "OrdinaryDiffEq.jl nonlinear diffusion" => "ode-diffusion1d.jl",
-            "OrdinaryDiffEq.jl 1D wave equation" => "ode-wave1d.jl",
-            "OrdinaryDiffEq.jl changing mass matrix" => "ode-nlstorage1d.jl",
-            #           "OrdinaryDiffEq.jl brusselator" => "ode-brusselator.jl",
-            "Coupling with Catalyst.jl" => "heterogeneous-catalysis.jl",
-            "Outflow boundary conditions" => "outflow.jl",
-            "Obtaining vector fields" => "flux-reconstruction.jl",
-            "Internal interfaces (1D)" => "interfaces1d.jl",
-            "A case for caution" => "problemcase.jl",
-            "Nonlinear solver control" => "nonlinear-solvers.jl",
-            #            "Bernoulli function test" => "bernoulli.jl",
-            #            "API Updates" => "api-update.jl",
-        ]
-        ENV["PLUTO_PROJECT"] = @__DIR__
-        notebook_examples = @docplutonotebooks(notebookdir, notebooks, iframe = false)
-        notebook_examples = vcat(["About the notebooks" => "notebooks.md"], notebook_examples)
-        size_threshold_ignore = last.(notebook_examples)
-        push!(pages, "Tutorial Notebooks" => notebook_examples)
-        @info "Notebook evaluation finished"
-    end
-
     if with_examples
-        @info "Start example evaluation"
+        @info "Start example documentation"
         modules = filter(
             ex -> splitext(ex)[2] == ".jl"
                 && occursin("Example", ex)
@@ -84,6 +60,33 @@ function make(;
         dev_examples = @docmodules(exampledir, devmodules, use_module_titles = true)
         push!(pages, "Development Examples" => dev_examples)
         @info "Example evaluation finished"
+        GC.gc()
+    end
+
+
+    if with_notebooks
+        @info "Start notebook evaluation"
+        notebooks = [
+            "OrdinaryDiffEq.jl nonlinear diffusion" => "ode-diffusion1d.jl",
+            "OrdinaryDiffEq.jl 1D wave equation" => "ode-wave1d.jl",
+            "OrdinaryDiffEq.jl changing mass matrix" => "ode-nlstorage1d.jl",
+            #           "OrdinaryDiffEq.jl brusselator" => "ode-brusselator.jl",
+            "Coupling with Catalyst.jl" => "heterogeneous-catalysis.jl",
+            "Outflow boundary conditions" => "outflow.jl",
+            "Obtaining vector fields" => "flux-reconstruction.jl",
+            "Internal interfaces (1D)" => "interfaces1d.jl",
+            "A case for caution" => "problemcase.jl",
+            # "Nonlinear solver control" => "nonlinear-solvers.jl",
+            # "Bernoulli function test" => "bernoulli.jl",
+            #            "API Updates" => "api-update.jl",
+        ]
+        ENV["PLUTO_PROJECT"] = @__DIR__
+        notebook_examples = @docplutonotebooks(notebookdir, notebooks, iframe = false, append_build_context = false)
+        notebook_examples = vcat(["About the notebooks" => "notebooks.md"], notebook_examples)
+        size_threshold_ignore = last.(notebook_examples)
+        push!(pages, "Tutorial Notebooks" => notebook_examples)
+        @info "Notebook evaluation finished"
+        GC.gc()
     end
 
     makedocs(;
